ccdaed24eb6ca29fe111c04d7c39d1e823c9e5ce
dragonloverlord <dragonloverlord@gmail.com>
2015-08-28 02:25:15 -0400

    added import for button4r1.cpp

--- row_one_functions.hpp
+++ row_one_functions.hpp
@@ -1,8 +1,9 @@
 #ifndef ROW_ONE_FUNCTIONS_HPP_INCLUDED
 #define ROW_ONE_FUNCTIONS_HPP_INCLUDED
 
-#include "button1r1.cpp"
-#include "button2r1.cpp"
-#include "button3r1.cpp"
+#include "row1/button1r1.cpp"
+#include "row1/button2r1.cpp"
+#include "row1/button3r1.cpp"
+#include "row1/button4r1.cpp"
 
 #endif
