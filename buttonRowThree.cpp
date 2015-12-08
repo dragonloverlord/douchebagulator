aaf98b06cd9923b0714eeca43f619fe209ae3189
dragonloverlord <dragonloverlord@gmail.com>
2015-12-08 13:14:41 -0500

    added function for button3

--- buttonRowThree.cpp
+++ buttonRowThree.cpp
@@ -12,7 +12,7 @@ public:
     {
         button_1(win, main_calc_box, button, display);
         button_2(win, main_calc_box, button, display);
-        button_3(win, main_calc_box, button);
+        button_3(win, main_calc_box, button, display);
         button_4(win, main_calc_box, button);
         button_5(win, main_calc_box, button);
     }
@@ -45,13 +45,17 @@ private:
         gtk_grid_attach(GTK_GRID(main_calc_box), button, 2, 3, 1, 1);
     }
 
-    static void button_3(GtkWidget *win, GtkWidget *main_calc_box, GtkWidget *button)
+    static void button_3(GtkWidget *win, GtkWidget *main_calc_box, GtkWidget *button, GtkWidget *display)
     {
         button = gtk_button_new_with_label("3");
+
+        g_object_set_data(G_OBJECT(button), "win", win);
+        g_object_set_data(G_OBJECT(button), "display", display);
+
         gtk_widget_set_hexpand(GTK_WIDGET(button), TRUE);
         gtk_widget_set_vexpand(GTK_WIDGET(button), TRUE);
         gtk_container_set_border_width(GTK_CONTAINER(button), 3);
-        g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(Calc().notDone), (gpointer) win);
+        g_signal_connect(GTK_BUTTON(button), "clicked", G_CALLBACK(Button3r3().main), (gpointer) button);
         gtk_grid_attach(GTK_GRID(main_calc_box), button, 3, 3, 1, 1);
     }
 
